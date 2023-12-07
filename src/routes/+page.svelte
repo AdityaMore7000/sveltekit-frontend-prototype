1765bf2a35bdff94323cb6456ff131119ccd59ac
adityamore7000 <adi.more7000@gmail.com>
2023-12-07 18:58:06 +0530

    change adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     export let data
-    import Loading from '../components/Loading.svelte'
-    import List from '../components/List.svelte'
+    import Loading from '../components/loading.svelte'
+    import List from '../components/list.svelte';
 </script>
 {#if data.status}
 <List {data} />
